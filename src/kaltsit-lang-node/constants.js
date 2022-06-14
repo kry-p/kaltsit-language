cdfd18cf4de13add444ff149c227eb80d80a38f9
kry-p <jhjung.dev@gmail.com>
2022-06-14 20:10:54 +0900

    first commit

---
+++ src/kaltsit-lang-node/constants.js
@@ -0,0 +1,8 @@
+export const CONTROL_STATEMENTS = ["그렇다면", "그럴수는 없다", "네가 원하는 답은 해 주지 않겠다"];
+export const CALCULATE_STATEMENTS = [
+  "그리 쉬운 것도 못 하나",
+  "그것도 못 하나",
+  "많은 일이 있었지",
+  "다음에 알려주겠다",
+  "지금은 때가 아니다",
+];
---
+++ src/kaltsit-lang-node/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-global-assign */
+// esm loader
+require = require("esm")(module);
+module.exports = require("./main.js");
